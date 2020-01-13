7a96cfe1eaf3577f262c48d03fae27f435589e20
Felipe Hermosilla Jorquera <fhermosilla@backboneing.com>
2020-01-13 13:03:18 -0300

    Carga instalación osTicket

---
+++ setup/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.4.0.min.js?933bb1f"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?933bb1f"></script>
+    <script type="text/javascript" src="js/setup.js?933bb1f"></script>
+    <script type="text/javascript" src="js/tips.js?933bb1f"></script>
+</body>
+</html>
